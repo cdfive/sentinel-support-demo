b4207e6f04b832b7b876e357226e0f916004897f
xiejihan <176402936@qq.com>
2018-08-31 17:12:38 +0800

    modify pom and logback.xml

--- activemq-demo/src/main/java/com/winxuan/sentinel/support/activemq/demo/Application.java
+++ activemq-demo/src/main/java/com/winxuan/sentinel/support/activemq/demo/Application.java
@@ -1,6 +1,5 @@
 package com.winxuan.sentinel.support.activemq.demo;
 
-
 import com.winxuan.sentinel.support.SentinelSupportConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
